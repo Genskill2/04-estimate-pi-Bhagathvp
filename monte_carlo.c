a9f6b72551fa4619586837a9b66b68cdd95896c2
Bhagathvp <84322745+Bhagathvp@users.noreply.github.com>
2021-05-30 15:05:02 +0530

    Add files via upload

--- monte_carlo.c
+++ monte_carlo.c
@@ -38,6 +38,13 @@ int main(void) {
     }
   }
 }
-
-
-
+float mc_pi(int count)
+{	float circount=0;
+	for(int i=0;i<count;i++)
+	{	float x=frandom();
+		float y=frandom();
+		if(sqrt((x*x)+(y*y))<1)
+		{	circount++;}}cd
+	float area=((4*circount)/count);
+	return area;}
+	
--- wallis.c
+++ wallis.c
@@ -1,3 +1,4 @@
+
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -24,3 +25,12 @@ int main(void) {
   }
 }
 
+float wallis_pi(int count)
+{
+	float pi=1.0;
+	for (int i=1;i<count;i++)
+	{	float num=4.0*i*i;
+		pi*=(num/(num-1));
+	}
+return pi*5;
+}
